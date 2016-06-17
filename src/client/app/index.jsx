831aa28a567738d6f47b816367e819affb66e267
Richard Young <richardy@discovery.co.za>
2016-06-17 16:39:56 +0200

    first commit

---
+++ src/client/app/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+
+import App from './modules/App.jsx'
+import About from './modules/About.jsx'
+import Repos from './modules/Repos.jsx'
+
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+        <Route path="/repos" component={Repos}/>
+        <Route path="/about" component={About}/>
+    </Router>
+), document.getElementById('app'));
