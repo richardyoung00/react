d7828e60294eb7b177a63ac49bfce0cc63a57407
Richard Young <richardy@discovery.co.za>
2016-06-21 15:27:11 +0200

    Add form

--- src/client/app/modules/Repos.jsx
+++ src/client/app/modules/Repos.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import NavLink from './NavLink.jsx'
 
 export default React.createClass({
+    // ask for `router` from context
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const userName = event.target.elements[0].value;
+        const repo = event.target.elements[1].value;
+        const path = `/repos/${userName}/${repo}`;
+        this.context.router.push(path)
+    },
+
     render() {
         return (
             <div>
@@ -10,6 +23,13 @@ export default React.createClass({
                 <ul>
                     <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
                     <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+                    <li>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" placeholder="userName"/> / {' '}
+                            <input type="text" placeholder="repo"/>{' '}
+                            <button type="submit">Go</button>
+                        </form>
+                    </li>
                 </ul>
 
                 {this.props.children}
