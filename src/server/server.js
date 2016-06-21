d1cbf8eb1c5f384edc8873940da7448300a477d0
Richard Young <richardy@discovery.co.za>
2016-06-21 15:04:54 +0200

    Split config. Compression.

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,14 @@
 var fallback = require('express-history-api-fallback');
 var express = require('express');
 var path = require("path");
+var compression = require('compression');
 var app = express();
-var root = __dirname + '/../client';
+app.use(compression());
+
+var root = __dirname + '/../client/public';
 
 app.use(express.static(root));
-app.use(fallback('index.html', { root: root }))
+app.use(fallback('index.html', { root: root }));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(root + '/index.html'));
